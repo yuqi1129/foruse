6ce9343664835a2307cca8cbdf42b31e965b8bed
hzyuqi1 <hzyuqi1@corp.netease.com>
2016-11-08 15:46:55 +0800

    新增测试用例

---
+++ src/main/java/main/TestOne.java
@@ -0,0 +1,35 @@
+package main;
+
+import com.google.common.collect.Sets;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created with foruse.
+ * User: hzyuqi1
+ * Date: 2016/11/8
+ * Time: 10:05
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class TestOne {
+
+    public static void main(String [] args){
+
+        Set<Integer> set1 = Sets.newTreeSet();
+
+        set1.add(1);
+        set1.add(3);
+
+        Set<Integer> set2 = Sets.newHashSet();
+        set2.add(3);
+        set2.add(4);
+
+        set2.retainAll(set1);
+
+        System.out.println(set2);
+
+
+    }
+}
